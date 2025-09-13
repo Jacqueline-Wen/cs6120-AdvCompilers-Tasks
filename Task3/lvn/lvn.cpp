0f9fa434aab744f08b373fc4f686a3dc859a87b7
Serena <huahuaserenazhang@gmail.com>
2025-09-13 14:03:01 -0400

    fix syntax issues

--- Task3/lvn/lvn.cpp
+++ Task3/lvn/lvn.cpp
@@ -8,52 +8,63 @@
 using json = nlohmann::json;
 using namespace std;
 
-bool localValueNumbering(json& j){
-    for (auto& function: j["functions"]){
+bool localValueNumbering(json &j)
+{
+    bool changed = false;
+    for (auto &function : j["functions"])
+    {
         map<vector<int>, pair<int, string>> table;
         map<string, int> var2num;
         int nextNum = 1;
 
-        auto& instrs = function["instrs"];
-        for (auto& intstr: instrs){
+        auto &instrs = function["instrs"];
+        for (auto &instr : instrs)
+        {
             // Building the value tuple
             vector<int> value;
             string op = instr["op"];
             value.push_back(hash<string>{}(op));
 
-            if(instr.contains("args")){
-                for (auto& a: instr["args"]){
+            if (instr.contains("args"))
+            {
+                for (auto &a : instr["args"])
+                {
                     string var = a;
-                    int num = var2num.count(var) ? var2num[var] : -1
+                    int num = var2num.count(var) ? var2num[var] : -1;
                     value.push_back(num);
-
                 }
             }
 
-            if (table.count(value)){
+            if (table.count(value))
+            {
                 int num = table[value].first;
                 string var = table[value].second;
 
                 instr = {
-                    {"op","id"},
+                    {"op", "id"},
                     {"args", json::array({var})},
-                    {"dest", instr["dest"]}
-                };
+                    {"dest", instr["dest"]}};
                 var2num[instr["dest"]] = num;
                 changed = true;
             }
-            else {
+            else
+            {
                 int num = nextNum++;
                 string dest = instr.contains("dest") ? string(instr["dest"]) : "";
                 table[value] = {num, dest};
-                
-                if (instr.contains("args")){
-                    for (auto& a: instr["args"]){
+
+                if (instr.contains("args"))
+                {
+                    for (auto &a : instr["args"])
+                    {
                         string var = a;
-                        if var2num.count(var){
+                        if (var2num.count(var))
+                        {
                             int tempNum = var2num[var];
-                            for (auto& kv : table){
-                                if (kv.second.first == tempNum) {
+                            for (auto &kv : table)
+                            {
+                                if (kv.second.first == tempNum)
+                                {
                                     a = kv.second.second;
                                     break;
                                 }
@@ -61,20 +72,25 @@ bool localValueNumbering(json& j){
                         }
                     }
                 }
-                if (!dest.empty()){
+                if (!dest.empty())
+                {
                     var2num[dest] = num;
                 }
             }
         }
     }
+
+    return changed;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     std::ifstream f(argv[1]);
     json j = json::parse(f);
 
-    bool notConverged = true
-    while (notConverged) {
+    bool notConverged = true;
+    while (notConverged)
+    {
         notConverged = localValueNumbering(j);
     }
 
