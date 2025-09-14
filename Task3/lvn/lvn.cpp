59eb86166190a1e771e22481627447644e361a76
Serena <huahuaserenazhang@gmail.com>
2025-09-13 22:18:42 -0400

    implement copy propagation

--- Task3/lvn/lvn.cpp
+++ Task3/lvn/lvn.cpp
@@ -8,13 +8,45 @@
 using json = nlohmann::json;
 using namespace std;
 
+void printVar2Num(const std::map<std::string, int> &var2num)
+{
+    std::cout << "Printing var2num map:" << std::endl;
+    for (const auto &pair : var2num)
+    {
+        std::cout << "  Key: " << pair.first << ", Value: " << pair.second << std::endl;
+    }
+}
+
+void printTable(const std::map<std::vector<int>, std::pair<int, std::string>> &table)
+{
+    std::cout << "Printing table map:" << std::endl;
+    for (const auto &entry : table)
+    {
+        // Print the key (the vector<int>)
+        std::cout << "  Key Vector: {";
+        for (size_t i = 0; i < entry.first.size(); ++i)
+        {
+            std::cout << entry.first[i];
+            if (i < entry.first.size() - 1)
+            {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "}";
+
+        // Print the value (the pair<int, string>)
+        std::cout << ", Value Pair: (" << entry.second.first << ", \"" << entry.second.second << "\")" << std::endl;
+    }
+}
+
 bool localValueNumbering(json &j)
 {
     bool changed = false;
+    map<vector<int>, pair<int, string>> table;
+    map<string, int> var2num;
+    std::set<std::string> commutative_ops = {"add", "mul", "eq", "ne", "and", "or"};
     for (auto &function : j["functions"])
     {
-        map<vector<int>, pair<int, string>> table;
-        map<string, int> var2num;
         int nextNum = 1;
 
         auto &instrs = function["instrs"];
@@ -24,9 +56,21 @@ bool localValueNumbering(json &j)
             vector<int> value;
             string op = instr["op"];
             value.push_back(hash<string>{}(op));
+            if (instr["op"] == "const")
+            {
+                value.push_back(instr["value"]);
+            }
 
             if (instr.contains("args"))
             {
+                if (commutative_ops.count(op))
+                {
+                    // For commutative ops with two args, sort the arg nums
+                    if (instr["args"][1] < instr["args"][0])
+                    {
+                        std::swap(instr["args"][1], instr["args"][0]);
+                    }
+                }
                 for (auto &a : instr["args"])
                 {
                     string var = a;
@@ -35,7 +79,22 @@ bool localValueNumbering(json &j)
                 }
             }
 
-            if (table.count(value))
+            if (instr["op"] == "id") // copy propagation
+            {
+                string arg = instr["args"][0];
+                string dest = instr["dest"];
+                var2num[dest] = var2num[arg];
+                int tempNum = var2num[arg];
+                for (auto &kv : table)
+                {
+                    if (kv.second.first == tempNum)
+                    {
+                        instr["args"][0] = kv.second.second;
+                        break;
+                    }
+                }
+            }
+            else if (table.count(value)) // instruction already exists
             {
                 int num = table[value].first;
                 string var = table[value].second;
@@ -79,14 +138,18 @@ bool localValueNumbering(json &j)
             }
         }
     }
+    // printTable(table);
+    // printVar2Num(var2num);
 
     return changed;
 }
 
 int main(int argc, char *argv[])
 {
-    std::ifstream f(argv[1]);
-    json j = json::parse(f);
+    // std::ifstream f(argv[1]);
+    // json j = json::parse(f);
+    json j;
+    std::cin >> j;
 
     bool notConverged = true;
     while (notConverged)
@@ -94,5 +157,6 @@ int main(int argc, char *argv[])
         notConverged = localValueNumbering(j);
     }
 
-    cout << j.dump(4) << endl;
+    std::cout << j.dump(4) << std::endl;
+    return 0;
 }
--- Task3/tdce/dce.cpp
+++ Task3/tdce/dce.cpp
@@ -8,34 +8,41 @@
 using json = nlohmann::json;
 using namespace std;
 
-bool removeReassign(json& j) {
+bool removeReassign(json &j)
+{
     bool changed = false;
-    for (auto& function: j["functions"]) {
+    for (auto &function : j["functions"])
+    {
         map<string, int> previousUsage;
         map<string, int> previousAssign;
         set<int> linesToRemove;
         int counter = 1;
-        for (auto instr: function["instrs"]){
+        for (auto instr : function["instrs"])
+        {
             // check for reassignment
-            if (!instr["dest"].is_null()) {
+            if (!instr["dest"].is_null())
+            {
                 string d = instr["dest"];
-                if (previousAssign[d] > previousUsage[d]) {
+                if (previousAssign[d] > previousUsage[d])
+                {
                     linesToRemove.insert(previousAssign[d]);
                 }
                 previousAssign[d] = counter;
             }
-            for (auto a: instr["args"]) {
+            for (auto a : instr["args"])
+            {
                 previousUsage[a] = counter;
             }
             counter++;
         }
 
-
         auto &instrs = function["instrs"];
 
         counter = 1;
-        for (auto it = instrs.begin(); it != instrs.end(); ++it) {
-            if (linesToRemove.find(counter) != linesToRemove.end()) {
+        for (auto it = instrs.begin(); it != instrs.end(); ++it)
+        {
+            if (linesToRemove.find(counter) != linesToRemove.end())
+            {
                 it = instrs.erase(it);
                 changed = true;
             }
@@ -45,22 +52,29 @@ bool removeReassign(json& j) {
     return changed;
 }
 
-bool removeUnusedVar(json& j) {
+bool removeUnusedVar(json &j)
+{
     bool changed = false;
-    for (auto& function: j["functions"]) {
+    for (auto &function : j["functions"])
+    {
         map<string, bool> varUsed;
-        for (auto instr: function["instrs"]) {
-            for (auto a: instr["args"]) {
+        for (auto instr : function["instrs"])
+        {
+            for (auto a : instr["args"])
+            {
                 varUsed[a] = true;
             }
         }
 
         auto &instrs = function["instrs"];
 
-        for (auto it = instrs.begin(); it != instrs.end(); ++it) {
-            if ((*it).contains("dest")){
+        for (auto it = instrs.begin(); it != instrs.end(); ++it)
+        {
+            if ((*it).contains("dest"))
+            {
                 string d = (*it)["dest"];
-                if (!varUsed[d]) {
+                if (!varUsed[d])
+                {
                     it = instrs.erase(it);
                     changed = true;
                     continue;
@@ -71,14 +85,17 @@ bool removeUnusedVar(json& j) {
     return changed;
 }
 
-
-int main(int argc, char* argv[]) {
-    std::ifstream f(argv[1]);
-    json j = json::parse(f);
+int main(int argc, char *argv[])
+{
+    // std::ifstream f(argv[1]);
+    // json j = json::parse(f);
+    json j;
+    std::cin >> j;
 
     bool notConverged = true;
 
-    while(notConverged) {
+    while (notConverged)
+    {
         notConverged = removeUnusedVar(j) || removeReassign(j);
     }
 
