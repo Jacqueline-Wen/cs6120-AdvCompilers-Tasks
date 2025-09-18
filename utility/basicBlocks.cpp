01f49c80e1639ad57ec4f54045f6838c247b9d5e
Jacqueline Wen <jw2347@cornell.edu>
2025-09-18 17:07:13 -0400

    implemented basic block functions

---
+++ utility/basicBlocks.cpp
@@ -0,0 +1,75 @@
+#include "basicBlocks.hpp"
+
+BasicBlocks::BasicBlocks(json &j) {
+    // creating basic blocks
+    int curLabel = 1;
+
+    queue<json> curBlock;
+    for (auto a : j["functions"]) {
+        for (auto instr : a["instrs"]) {
+            // end block
+            if (instr.contains("op") &&
+                (instr["op"] == "br" || instr["op"] == "jmp" ||
+                 instr["op"] == "ret")) {
+                curBlock.push(instr);
+
+                while (curBlock.size() > 0) {
+                    blocks_[curLabel].push_back(curBlock.front());
+                    curBlock.pop();
+                }
+                curLabel++;
+            } else if (instr.contains("label")) {
+                if (curBlock.size() > 0) {
+                    while (curBlock.size() > 0) {
+                        blocks_[curLabel].push_back(curBlock.front());
+                        curBlock.pop();
+                    }
+                    curLabel++;
+                    curBlock.push(instr);
+                }
+                blockToLabelName_[curLabel] = instr["label"];
+                labelNameToBlock_[instr["label"]] = curLabel;
+            } else {
+                curBlock.push(instr);
+            }
+        }
+    }
+    while (curBlock.size() > 0) {
+        blocks_[curLabel].push_back(curBlock.front());
+        curBlock.pop();
+    }
+
+    // populating links
+    bool saveNext = false;
+    int prev = -1;
+    for (auto a: blocks_) {
+        int blockname = a.first;
+        auto lastCommand = a.second[a.second.size()-1];
+        if (saveNext) {
+            predecessors_[blockname].push_back(prev);
+            successors_[prev].push_back(blockname);
+        }
+        if(lastCommand.contains("labels")) {
+            for (auto children: lastCommand["labels"]) {
+                int childInt = labelNameToBlock_[children];
+                successors_[blockname].push_back(childInt);
+                predecessors_[childInt].push_back(blockname);
+            }
+        } else {
+            saveNext = true;
+            prev = blockname;
+        }
+    }
+}
+
+map<int, vector<json>> BasicBlocks::getBlocks() {
+    return blocks_;
+}
+
+vector<int> BasicBlocks::getPredecessors(int label) {
+    return predecessors_[label];
+}
+
+vector<int> BasicBlocks::getSuccessors(int label) {
+    return successors_[label];
+}
---
+++ utility/basicBlocks.hpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <nlohmann/json.hpp>
+#include <set>
+#include <vector>
+#pragma once
+
+using json = nlohmann::json;
+using namespace std;
+
+class BasicBlocks {
+public:
+    BasicBlocks(json& j);
+    map<int, vector<json>> getBlocks();
+    vector<int> getPredecessors(int label);
+    vector<int> getSuccessors(int label);
+
+private:
+    map<int, vector<int>> predecessors_;
+    map<int, vector<int>> successors_;
+    map<int, vector<json>> blocks_;
+    map<int, string> blockToLabelName_;
+    map<string, int> labelNameToBlock_;
+} 
