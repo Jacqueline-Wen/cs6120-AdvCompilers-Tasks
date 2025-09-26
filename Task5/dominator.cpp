a7d7a2e0f864bf2203b473c17bb52f9a94b0f207
Serena <huahuaserenazhang@gmail.com>
2025-09-26 03:23:56 -0400

    implement find dominator

---
+++ Task5/dominator.cpp
@@ -0,0 +1,102 @@
+#include "../utility/basicBlocks.hpp"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <set>
+#include <map>
+
+using namespace std;
+using json = nlohmann::json;
+
+void printDominators(const map<int, set<int>> &dom)
+{
+    for (const auto &pair : dom)
+    {
+        int blockLabel = pair.first;
+        cout << "Block " << blockLabel << " is dominated by: ";
+        for (int dominator : pair.second)
+        {
+            cout << dominator << " ";
+        }
+        cout << endl;
+    }
+}
+
+map<int, set<int>> findDominators(shared_ptr<BasicBlocks> basicBlocks)
+{
+    auto blocks = basicBlocks->getBlocks();
+    map<int, set<int>> dom;
+    if (blocks.empty())
+    {
+        return dom;
+    }
+    int entry = blocks.begin()->first;
+
+    // dom = {every block -> all blocks}
+    set<int> allBlocks;
+    for (const auto &block : blocks)
+    {
+        allBlocks.insert(block.first);
+    }
+    for (const auto &block : blocks)
+    {
+        int blockLabel = block.first;
+        dom[blockLabel] = allBlocks;
+    }
+
+    // dom[entry] = {entry}
+    dom[entry] = {entry};
+    bool changed = true;
+    while (changed)
+    {
+        changed = false;
+        for (const auto &block : blocks)
+        {
+            int blockLabel = block.first;
+            if (blockLabel == entry)
+            {
+                continue;
+            }
+
+            // dom[vertex] = {vertex} ∪ ⋂(dom[p] for p in vertex.preds}
+            set<int> oldDom = dom[blockLabel];
+            vector<int> preds = basicBlocks->getPredecessors(blockLabel);
+            set<int> newDom;
+            if (!preds.empty())
+            {
+                newDom = dom[preds[0]];
+                for (size_t i = 1; i < preds.size(); i++)
+                {
+                    set<int> temp;
+                    int pred = preds[i];
+                    for (int d : dom[pred])
+                    {
+                        if (newDom.find(d) != newDom.end())
+                        {
+                            temp.insert(d);
+                        }
+                    }
+                    newDom = std::move(temp);
+                }
+            }
+            newDom.insert(blockLabel);
+            if (newDom != oldDom)
+            {
+                dom[blockLabel] = newDom;
+                changed = true;
+            }
+        }
+    }
+    return dom;
+}
+
+int main(int argc, char *argv[])
+{
+    json j;
+    cin >> j;
+
+    shared_ptr<BasicBlocks> basicBlocks = make_shared<BasicBlocks>(j);
+    map<int, set<int>> dominators = findDominators(basicBlocks);
+    printDominators(dominators);
+}
